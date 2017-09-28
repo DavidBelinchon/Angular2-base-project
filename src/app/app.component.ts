6330e709f92c46baa9a58d146ce37a8191e70eff
David <david.belinchon@gft.com>
2017-09-28 15:27:28 +0200

    footter styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: 
+  styleUrls: ['./styles/app.component.css'],
+  template:
   `
     <navbar></navbar>
 	<!-- {{title}} -->
+	<div class="container-fluid">
   	<router-outlet></router-outlet>
+	</div>
+	<section class="footer">
+		<div class="container-fluid">
+			<div class="row">
+				<p>
+					Miaaaaau!
+				</p>
+				<br>
+			</div>
+		</div>
+	</section>
   `
 })
 
